94d5191db0ac3aecab0cd04a2e917c5210d863a8
TEJAS-SINGH5032008 <86147739+TEJAS-SINGH5032008@users.noreply.github.com>
2021-07-25 19:52:05 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,63 +1,36 @@
+
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
 
-var engine, world;
-var box1, pig1;
-var base;
-var backgroundImg;
 
-function preload(){
-backgroundImg = loadImage("sprites/bg.png");
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
 
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+
+	Engine.run(engine);
+  
 }
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
+  groundObject.display();
+  dustbinObj.display();
 
 }
 
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-}
